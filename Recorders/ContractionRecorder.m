7f250be30601c0714a1ab2a7e68b40838d9269ff
dovecho <dovecho@gmail.com>
2015-11-12 22:45:04 +0800

    Init of Recorders and ContractionRecorder
    ContractionRecorder

---
+++ Recorders/ContractionRecorder.m
@@ -0,0 +1,85 @@
+function ContractionRecorder
+%CONTRACTION RECORDER
+%   ContractionRecorder is written to record the contraction time and
+%   interval for a parturient who is about to give birth. The regular
+%   uterine contraction indicates the family should prepared to go to
+%   hospital at any time when the contraction intervals meet the
+%   requirement that you have to action.
+%
+%   Using the program is easy, just RUN and press ANY KEY while record the
+%   start time and stop time of one contraction.
+
+%	Written by dovecho at GMAIL dot COM
+%   File name: ContractionRecorder.m
+%	Revision Note: First publish of the program
+%	$Version: 1.0.0.1 $	$Date: 2015-11-12 22:34:48 $
+
+% Recording
+recordFName = sprintf('REC_CONTRACTION_%s.txt', datestr(now, 'yyyy-mm-dd'));
+diary(recordFName);
+
+% set clean up function, to close diary while force excuting
+% ref: http://cn.mathworks.com/help/matlab/ref/oncleanup.html
+diaryclearnup = onCleanup(@() set(0,'Diary','off'));
+
+strWait1 = 'Press ANY KEY when contraction STARTS... ';
+strWait2 = 'Press ANY KEY when contraction STOPS... ';
+
+% Start Recording
+global iContractionRecorderCount;
+global tContractionRecorder;
+
+if isempty(iContractionRecorderCount)
+    
+    iContractionRecorderCount = 1;
+    tContractionRecorder = 0;
+    
+    fprintf('Welcome to CONTRACTION RECORDER!\n');
+    fprintf('======================================\n');
+    fprintf('WARNING!!! Be prepared for delivery before using this program!!\n\n')
+    fprintf('When contraction interval meets the requirement to go to the hospital,\n');
+    fprintf('GO at ease, and good luck!\n\n');
+    fprintf('NOTE1: Press CTRL+C to terminate the recording.\n');
+    fprintf('NOTE2: You can run the program again to continue recording.\n');
+    fprintf('NOTE3: Run CLEAR ALL to reset the recording.\n\n');
+    fprintf('RECORDING STARTS AT: %s\n', datestr(now));
+    fprintf('=======================================================\n');
+end
+
+% Display information while waiting a contraction starts
+diary off;fprintf('%s\n', strWait1);pause;diary on;
+
+% Start timing of contraction
+tic;
+
+% Main loop
+while 1
+    % Record the starting time of a contraction
+    diary off;fprintf('\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b');diary on;
+    strStartTime = datestr(now, 'HH:MM:SS'); 
+    dt = toc+tContractionRecorder; % DT is the time interval between two contractions
+    
+    fprintf('No. %3d Starts at: %s, ', iContractionRecorderCount, strStartTime);
+    if iContractionRecorderCount > 1
+        fprintf('Interval: %02d:%02d, ', floor(dt/60), mod(round(dt),60));
+    end
+    
+    % Wait for contraction ends
+    tic;
+    diary off;fprintf('\n%s', strWait2);diary on;
+    pause;
+    
+    % Contraction ends
+    diary off;fprintf('\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b');diary on;
+    strEndTime =  datestr(now);
+    tContractionRecorder = toc;
+    fprintf('Last: %d sec.\n', round(tContractionRecorder));
+    
+    % Wait for next contraction starts
+    tic;
+    iContractionRecorderCount = iContractionRecorderCount + 1;
+    
+    diary off;fprintf('%s\n', strWait1);pause;diary on;
+end
+
+diary off;
---
+++ Recorders/README.txt
@@ -0,0 +1,5 @@
+README of RECORDERs in DaylyFractions
+
+File name		Date of first commit	Note
+====================================================================
+ContractionRecorder.m	22:41 2015/11/12	Record Contraction
